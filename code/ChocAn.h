6ebb8a698c59924c2e53bb47de910478ee610e87
Eddie Yefremov <yefremov@pdx.edu>
2024-02-27 17:33:20 -0800

    Added all the .txt files and cpp files + header

---
+++ code/ChocAn.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Interface {
+    public:
+        Interface();
+
+    private:
+};
+
+class Manager {
+    public:
+        Manager();
+
+    private:
+};
+
+class Provider {
+    public:
+        Provider();
+
+    private:
+};
+
+class Service {
+    public:
+        Service();
+
+    private:
+};
---
+++ code/main.cpp
@@ -0,0 +1,7 @@
+#include "ChocAn.h"
+
+
+int main() {
+    cout << "Welcome to ChocAn!" << endl;
+    return 0;
+}

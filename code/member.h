9fcb757aae66cfd071516350bb43e5d48b0ca559
Eddie Yefremov <yefremov@pdx.edu>
2024-02-27 19:32:27 -0800

    More additions to the member.h and provider.h header files

--- code/member.h
+++ code/member.h
@@ -3,5 +3,12 @@
 class Member {
     public:
         Member();
+        bool set_member_info(char name[], int ID[], char addr[], char city[], char state[], int zipcode[]);
     private:
+        char member_name[25];
+        int member_ID[9];
+        char member_street_addr[25];
+        char member_city[14];
+        char member_state[2];
+        int member_zipcode[5];
 };
--- code/provider.h
+++ code/provider.h
@@ -5,6 +5,14 @@
 class Provider {
     public:
         Provider();
+        bool set_provider_info(char name[], int ID[], char addr[], char city[], char state[], int zipcode[]);
     private:
         Service current_service;
+        char provider_name[25];
+        int provider_ID[9];
+        char street_addr[25];
+        char provider_city[14];
+        char provider_state[2];
+        int provider_zipcode[5];
+
 };

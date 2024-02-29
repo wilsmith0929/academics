428c8c1df571ba3d1052ed2edd0f79ee258635bc
Kevin Phan <keviphan@cecs.pdx.edu>
2024-02-29 03:12:39 +0000

    added some side utilities we might use

--- code/utilities.cpp
+++ code/utilities.cpp
@@ -1,3 +1,44 @@
 #include "utilities.h"
 
-//implementation of side functions
+int readInt() {
+  //temp integer to be inputted
+  int temp = 0;
+  cin >> temp;
+  while(!cin || temp <= 0) {
+    cin.clear();
+    cin.ignore(MAX, '\n');
+    cout << "Invalid input! Try again: ";
+    cin  >> temp;
+  }
+  cin.ignore(MAX, '\n');
+  return temp;
+}
+
+char readChar() {
+  //temp char to be inputted
+  char temp = '\0';
+  cin >> temp;
+  while(!cin) {
+    cin.clear();
+    cin.ignore(MAX, '\n');
+    cout << "Invalid character! Try again: ";
+    cin  >> temp;
+  }
+  cin.ignore(MAX, '\n');
+  return temp;
+}
+
+double getDouble() {
+  //temp double to be inputted
+  double temp = 0;
+  cin >> temp;
+  while(!cin || temp <= 0) {
+    cin.clear();
+    cin.ignore(MAX, '\n');
+    cout << "Invalid input! Try again: ";
+    cin  >> temp;
+  }
+  cin.ignore(MAX, '\n');
+  return temp;
+}
+
--- code/utilities.h
+++ code/utilities.h
@@ -4,4 +4,11 @@
 #include <string>
 using namespace std;
 
+const int MAX = 101;
+
 //other side functions we might use
+
+//just some functions that might be useful for inputting, it does data validation
+double getDouble();
+int    readInt();
+char   readChar();

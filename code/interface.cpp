5e7031238290a187c544d9e4326f642d6658f117
Kevin Phan <keviphan@pdx.edu>
2024-03-04 17:25:27 -0800

    KP - implemented some of the itnerface functions - JUST STARTERS, temp for now

--- code/interface.cpp
+++ code/interface.cpp
@@ -32,27 +32,139 @@ void begin() {
     displayExitMsg();
 }
 
-void providerMenu() {
-    cout << "1- Validate Members" << endl;
-    cout << "2- Enter Service Details" << endl;
-    cout << "3- View Service History" << endl;
-    cout << "4- View Service Directory" << endl;
-    cout << "5- Return to Main Menu" << endl;
-}
+//KP - execute the options of user, we'll update the vectors to vector<User> respectively 
+void exeCmd(int option, vector<Member> & memberList, vector<Provider> & providerList) {
+    int userOption = 0;
 
-void managerMenu() {
-    cout << "1- Manage Members" << endl;
-    cout << "2- Manage Providers" << endl;
-    cout << "3- Generate Provider Report" << endl;
-    cout << "4- Generate Member Report" << endl;
-    cout << "5- Return to Main Menu" << endl;
+    //KP - using nested switches
+    //feel free to change
+    //THESE ARE JUST TO GET US STARTED
+
+    switch(option) {
+        
+        //manager login, then if valid we prompt them with manager menu
+        case 1:
+            int password = 0;
+            //check if valid manager
+            cout << "Manager Login!" << endl;
+            cout << "Enter manager password: ";
+            cin  >> password;
+
+            // we can maybe have a utility functin that checks for password
+            //just a temporary for now, PASSWORD is global in utilities.h
+            //if valid, proceed with manager menu
+            if(password == PASSWORD) {
+                do {
+                    managerMenu();
+                    cout << "\n\t=> Option: ";
+                    userOption = read_int();
+
+                    switch(userOption) {
+                        case 1:
+                            cout << "manager menu action 1" << endl;
+                            break;
+                        case 2:
+                            cout << "manager menu action 2" << endl;
+                            break;
+                        case 3:
+                            cout << "manager menu action 3" << endl;
+                            break;
+                        case 4:
+                            cout << "manager menu action 4" << endl;
+                            break;
+                        case 5:
+                            cout << "\n\tExiting Manager Menu, back to main menu" << endl;
+                            break;
+                        default:
+                            cout << "\n\tInvalid Option! Please try again! : " << endl;
+                    }
+                } while(userOption != 5);
+            }
+            else {
+                //we can prompt them to try again or just break back to main menu
+                break;
+            }
+
+        //check if valid provider, then prompt providerMenu
+        case 2:
+            int password = 0;
+            //check if valid provider
+            cout << "Provider Login!" << endl;
+            cout << "Enter provider password: ";
+            cin  >> password;
+
+            // we can maybe have a utility functin that checks for password
+            //just a temporary for now, PASSWORD is global in utilities.h
+            //if valid, proceed with manager menu
+            if(password == PASSWORD) {
+                do {
+                    providerMenu();
+                    cout << "\n\t=> Option: ";
+                    userOption = read_int();
+
+                    switch(userOption) {
+                        case 1:
+                            cout << "provider menu action 1" << endl;
+                            break;
+                        case 2:
+                            cout << "provider menu action 2" << endl;
+                            break;
+                        case 3:
+                            cout << "provider menu action 3" << endl;
+                            break;
+                        case 4:
+                            cout << "provider menu action 4" << endl;
+                            break;
+                        case 5:
+                            cout << "\n\tExiting Provider Menu, back to main menu" << endl;
+                            break;
+                        default:
+                            cout << "\n\tInvalid Option! Please try again! : " << endl;
+                    }
+                } while(userOption != 5);
+            }
+            else {
+                //we can prompt them to try again or just break back to main menu
+                break;
+            }
+
+        
+        case 3:
+            cout << "Exiting Main Menu!" << endl;
+            break;
+        default:
+            cout << "Invalid Option! Please try again! : " << endl;
+
+    }
 }
 
 void displayMainMenu() {
-    cout << "1- Manager Login" << endl;
-    cout << "2- Provider Login" << endl;
-    cout << "3- Quit" << endl;
+    cout << "\n** Main Menu **" << endl;
+    cout << "\t1- Manager Login" << endl;
+    cout << "\t2- Provider Login" << endl;
+    cout << "\t3- Quit" << endl;
 }
+
+void providerMenu() {
+    cout << "\n\t** Provider Menu **" << endl;
+    cout << "\t\t1- Validate Members" << endl;
+    cout << "\t\t2- Enter Service Details" << endl;
+    cout << "\t\t3- View Service History" << endl;
+    cout << "\t\t4- View Service Directory" << endl;
+    cout << "\t\t5- Return to Main Menu" << endl;
+}
+
+//KP - lmk if we need to remove the tabs, just doing it cuz it'll be easier to naviagte 
+//later on
+void managerMenu() {
+    cout << "\t\t1- Manage Members" << endl;
+    cout << "\t\t2- Manage Providers" << endl;
+    cout << "\t\t3- Generate Provider Report" << endl;
+    cout << "\t\t4- Generate Member Report" << endl;
+    cout << "\t\t5- Return to Main Menu" << endl;
+}
+
+
 
 void displayWelcomeMsg() {
     cout << "Welcome to the ChocAn interface!" << endl;
--- code/interface.h
+++ code/interface.h
@@ -2,13 +2,13 @@
 #include "utilities.h"
 #include "manager.h"
 #include "member.h" //KP - for now just inlcuding these, will update later with 
-                    //the "user" class
+                    //the "user.h" class
 
 void begin();               //KP - act as main, we will call this in main
-void displayMainMenu();            //KP - main menu
+void displayMainMenu();     //KP - main menu
 
 //KP - execute the options of user, we'll update the vectors to vector<User> respectively 
-void exeCmd(char option, vector<Member> & memberList, vector<Provider> & providerList);
+void exeCmd(int option, vector<Member> & memberList, vector<Provider> & providerList);
 void providerMenu();        //KP - provider menu mimic test plan
 void managerMenu();         //KP - same as above^
 

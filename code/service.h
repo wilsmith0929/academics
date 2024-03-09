664a8f9fc8fdc48e0d3b161b0e30e057c9fe45c4
William Smith <wilsmith@cecs.pdx.edu>
2024-03-09 23:31:26 +0000

    Updated header file

--- code/service.h
+++ code/service.h
@@ -5,7 +5,12 @@
 class Service {
     public:
         Service();
+        int display_services();
 
     private:
+        int service_code;
+        string service_name;
+        int service_fee;
+
 
 };

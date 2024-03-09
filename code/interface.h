fb7af4ea33440f5313f8b011ae29b7591db5f62d
William Smith <wilsmith@cecs.pdx.edu>
2024-03-09 23:22:55 +0000

    Put into class and a vector as private data, can be changed later

--- code/interface.h
+++ code/interface.h
@@ -4,13 +4,19 @@
 #include "member.h" //KP - for now just inlcuding these, will update later with 
                     //the "user.h" class
 
-void begin();               //KP - act as main, we will call this in main
-void displayMainMenu();     //KP - main menu
-
-//KP - execute the options of user, we'll update the vectors to vector<User> respectively 
-void exeCmd(int option, vector<Member> & memberList, vector<Provider> & providerList);
-void providerMenu();        //KP - provider menu mimic test plan
-void managerMenu();         //KP - same as above^
-
-void displayWelcomeMsg();   //KP - welcome msg
-void displayExitMsg();      //KP - exit msg
+//William - added these in a class, put vectors in the private section, can update once the people
+//class is created
+class Interface
+    public:
+        Interface();
+        void begin();               //KP - act as main, we will call this in main
+        void displayMainMenu();     //KP - main menu
+        //KP - execute the options of user, we'll update the vectors to vector<User> respectively 
+        void exeCmd(int option, vector<Member> & memberList, vector<Provider> & providerList);
+        void providerMenu();        //KP - provider menu mimic test plan
+        void managerMenu();         //KP - same as above^
+        void displayWelcomeMsg();   //KP - welcome msg
+        void displayExitMsg();      //KP - exit msg
+    private:
+        vector<Member> & memberList;
+        vector<Provider> & providerList;

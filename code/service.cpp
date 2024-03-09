51677dad08eafa159301e1e939baf1c92a6ef23d
William Smith <wilsmith@cecs.pdx.edu>
2024-03-09 23:30:33 +0000

    Function to display the services provided. Feel free to change

--- code/service.cpp
+++ code/service.cpp
@@ -1,3 +1,33 @@
 #include "service.h"
 
 Service::Service() {}
+
+int Service::display_services()
+{
+    ifstream inputFile("services.txt");
+
+    if (!inputFile.is_open()) 
+    {
+        cerr << "Error opening the file" << endl;
+        return 1;
+    }
+
+    string line;
+    while(getline(inputFile, line)) 
+    {
+        istringstream iss(line);
+        
+        if (iss >> service_code >> ws && getline(iss, service_name, '|') && iss >> service_fee)
+        {
+            cout << "Service Code: " << service_code << " Service Name: " << service_name << " Service Fee: " << service_fee << endl;
+        }
+
+        else {
+            cerr << "Error parsing line" << line << endl;
+        }
+    }
+
+    inputFile.close();
+
+    return 1;
+}

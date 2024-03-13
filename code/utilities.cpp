0d94ecec7add79106d6f8d56a80a8a6314a832b9
Azeezah <aacar@pdx.edu>
2024-03-12 17:52:39 -0700

    Added the loading function to utilities

--- code/utilities.cpp
+++ code/utilities.cpp
@@ -42,3 +42,69 @@ double get_double() {
   return temp;
 }
 
+long upload_people(PersonVec & members, PersonVec & providers, PersonVec & managers)
+{
+        string filename = PEOPLE_FILE;
+        ifstream fin;
+
+        long highest_ID = 0;
+
+        long ID = 0;
+        string id_buf = "";
+        string status = "";
+        string type = "";
+        string first_name = "";
+        string last_name = "";
+        string street_add = "";
+        string city = "";
+        string state = "";
+        int zip_code = 0;
+
+        fin.open(filename.c_str());
+        if (!fin) return 1;
+        else
+        {
+                while(!fin.eof())
+                {
+                        getline(fin, id_buf, '|');
+                        getline(fin, first_name, '|');
+                        getline(fin, last_name, '|');
+                        getline(fin, status, '|');
+                        getline(fin, type, '|');
+                        getline(fin, street_add, '|');
+                        getline(fin, city, '|');
+                        getline(fin, state, '|');
+
+                        fin >> zip_code;
+                        fin.ignore(20, '\n');
+
+                        ID = stol(id_buf);
+
+
+                        if (type.compare(MAN_TYPE) == 0)
+                        {
+                                managers.upload_person(ID, status, type, first_name, last_name, street_add, city, state, zip_code);
+                        }
+                        else if (type.compare(MEM_TYPE) == 0)
+                        {
+                                members.upload_person(ID, status, type, first_name, last_name, street_add, city, state, zip_code);
+                        }
+                        else
+                        {
+                                providers.upload_person(ID, status, type, first_name, last_name, street_add, city, state, zip_code);
+                        }
+
+                        if (ID > highest_ID)
+
+                        {
+                                highest_ID = ID;
+                        }
+
+                        fin.peek();
+                }
+        }
+
+        fin.close();
+
+        return highest_ID;
+}
--- code/utilities.h
+++ code/utilities.h
@@ -10,6 +10,11 @@
 #include <cstdlib>
 using namespace std;
 
+#define MEM_TYPE "MEMBER"
+#define PRO_TYPE "PROVIDER"
+#define MAN_TYPE "MANAGER"
+#define PEOPLE_FILE "people.txt"
+
 const int MAX = 101;
 const int _PASSWORD = 1111;
 
@@ -19,3 +24,8 @@ const int _PASSWORD = 1111;
 double get_double();
 int    read_int();
 char   read_char();
+
+//The prototype for the function that uploads all members, providers, and managers. 
+//Returns the largest ID. I was thinking that whenever we create a new member we just increment the
+//largest value.
+long upload_people(PersonVec & members, PersonVec & providers, PersonVec & managers);
